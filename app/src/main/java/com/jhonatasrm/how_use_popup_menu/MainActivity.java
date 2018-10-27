1b7d1165cbfce8f9c82d0a2ef541f26da6c6df98
Jhonatas Rodrigues <jhonatasrm@gmail.com>
2018-10-27 17:13:26 -0200

    Initial commit

---
+++ app/src/main/java/com/jhonatasrm/how_use_popup_menu/MainActivity.java
@@ -0,0 +1,35 @@
+package com.jhonatasrm.how_use_popup_menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+
+    public void startPopupMenu(View view) {
+        PopupMenu popupMenu = new PopupMenu(this, view);
+        popupMenu.inflate(R.menu.popup_menu);
+        popupMenu.setOnMenuItemClickListener(this);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        if(menuItem.getItemId() == R.id.next){
+            Toast.makeText(this, "NEXT", Toast.LENGTH_SHORT).show();
+            return true;
+        }else if (menuItem.getItemId() == R.id.previous){
+            Toast.makeText(this, "PREVIOUS", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
+    }
+}
