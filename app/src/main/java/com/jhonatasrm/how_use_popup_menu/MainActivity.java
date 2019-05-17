e7d4fee3c4d072360425f9a4c9ca0780b7626000
Jhonatas Rodrigues <jhonatasrm@gmail.com>
2019-05-17 16:58:15 -0300

    Update MainActivity.java

--- app/src/main/java/com/jhonatasrm/how_use_popup_menu/MainActivity.java
+++ app/src/main/java/com/jhonatasrm/how_use_popup_menu/MainActivity.java
@@ -7,20 +7,24 @@ import android.view.View;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
+// implementando onMenuItemClickListener para o PopupMenu
 public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
+    // método onCreate da classe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
     }
 
+    // inicializa o PopupMenu
     public void startPopupMenu(View view) {
         PopupMenu popupMenu = new PopupMenu(this, view);
         popupMenu.inflate(R.menu.popup_menu);
         popupMenu.setOnMenuItemClickListener(this);
     }
 
+    // verifica o item clicado no menu exibindo um toast com a mensagem
     @Override
     public boolean onMenuItemClick(MenuItem menuItem) {
         if(menuItem.getItemId() == R.id.next){
